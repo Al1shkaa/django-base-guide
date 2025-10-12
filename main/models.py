4f8052d398e95f89f6ed877aeaa9b4f7ef376430
username <mail@ex.com>
2025-10-12 14:49:53 +0500

    Сделал views,urls настроил и написал шаблоны каталога и товаров

--- main/models.py
+++ main/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 
 class Category(models.Model):
     name = models.CharField(max_length=100, db_index=True)
@@ -12,6 +12,10 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse("main:product_list_by_category", args=[self.slug])
+    
 
 
 class Product(models.Model):
@@ -36,3 +40,7 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse("main:product_detail", args=[self.id ,self.slug])
+    
